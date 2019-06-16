bd783c7d439a95c02cdb6f10f1b2b5a04491b7fc
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-06-16 13:39:01 -0400

    Add README.md and usage message

---
+++ README.md
@@ -0,0 +1,12 @@
+# Morris Traversal
+
+Implementation of [Morris Traversal](https://en.wikipedia.org/wiki/Tree_traversal#Morris_in-order_traversal_using_threading)
+of a binary tree. Morris traversal is an in-order traversal without recursion
+(i.e.  using O(1) space), which leverages [threading](https://en.wikipedia.org/wiki/Threaded_binary_tree).
+Time-complexity is O(n), where n is the number of nodes.
+
+## Usage
+
+```
+Usage: morris-traversal <int>...
+```
--- main.go
+++ main.go
@@ -2,21 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
-func main() {
-	var t Tree
-	for _, arg := range os.Args[1:] {
-		i, err := strconv.Atoi(arg)
-		if err != nil {
-			log.Fatalf("Invalid int: %s", arg)
-		}
-		t.Add(i)
+func usage() {
+	fmt.Printf("Usage: %s <int>...\n", os.Args[0])
+	os.Exit(2)
+}
+
+func parseArgs() []int {
+	if len(os.Args) == 1 {
+		usage()
 	}
 
+	var ints []int
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-h", "--help":
+			usage()
+		}
+
+		i, err := strconv.Atoi(arg)
+		if err != nil {
+			fmt.Printf("Invalid int: %s\n", arg)
+			usage()
+		}
+		ints = append(ints, i)
+	}
+	return ints
+}
+
+func main() {
+	var t Tree
+	for _, arg := range parseArgs() {
+		t.Add(arg)
+	}
 	results := t.Traverse()
 	fmt.Println(results)
 }
